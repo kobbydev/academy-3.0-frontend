2a8d90a39ab1fe74fc43fd1156c31c5a42de1dd2
kobbydev <antwiagyapong12@gmail.com>
2022-08-12 12:33:20 +0000

    adding fonts

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,32 @@
 <template>
-  <router-view />
-
+	<router-view />
 </template>
 
 <script>
-
-
 export default {
-  name: 'App',
-  components: {
-  }
-}
+	name: 'App',
+	components: {},
+};
 </script>
 
 <style>
-#app {
+/* #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
+} */
+@font-face {
+	font-family: 'Lato';
+	src: local('Lato'), url(./fonts/Lato-Regular.ttf) format('truetype');
 }
-
+@font-face {
+	font-family: 'Poppins';
+	src: local('Poppins'), url(./fonts/Poppins-Regular.ttf) format('truetype');
+}
 * {
-  margin: 0;
-  padding: 0;
-  box-sizing: border-box;
+	margin: 0;
+	padding: 0;
+	box-sizing: border-box;
 }
 </style>
