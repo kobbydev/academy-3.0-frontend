f3372fb9d127b420a93bb3368794f65320c937e7
Edem <edem@DESKTOP-F3NTFMN>
2022-09-02 15:43:48 +0000

    Sorting

---
+++ src/views/admin-views/Sorting.vue
@@ -0,0 +1,52 @@
+<template lang="">
+    <div>
+        <table>
+            <thead>
+                <tr>
+                    <th v-for="key of columns">{{key}}</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for = "entry in filteredFilms">
+                    <td v-for = "key of columns">
+                        {{entry[key]}}
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</template>
+<script>
+export default {
+    data(){
+        return{
+            column: ['title', 'rating'],
+            ratingInfo: [
+                {title: Malcolm, rating: 94},
+                {title: Kwabena, rating: 93},
+                {title: Edem, rating: 90},
+            ],
+            filterText: ''
+        }
+    },
+    methods: {
+        sortLowest(){
+            this.ratingInfo.sort((a,b)=>a.rating > b.rating? 1: -1
+            );
+        },
+            sortHighest(){
+            this.ratingInfo.sort((a,b)=>a.rating < b.rating? 1: -1
+            );
+        }
+    },
+    computed:{
+        filteredFilms(){
+            let film = new RegExp(this.filterText, 'i')
+            return this.ratingInfo.filter(el=>el.title.match(filter))
+        }
+    }
+ }
+</script>
+<style lang="">
+    
+</style>
